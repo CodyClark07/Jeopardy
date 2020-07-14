81a79de2a9881febdd5f5001039ca5be82b10526
CodyClark07 <codyclark2010@hotmail.com>
2020-07-13 20:28:51 -0600

    edit columns

--- app/Models/Value.js
+++ app/Models/Value.js
@@ -6,7 +6,7 @@ export default class Value {
 
     get Template() {
         return /* html */`
-        <div id="shake" class="col-4 border rounded border-danger shadow-lg p-4 card">
+        <div id="shake" class="col-12 col-md-6 border rounded border-danger shadow-lg p-4 card">
         <h3>${this.question}</h3>
         <button class="btn btn-primary" onclick="app.valuesController.getAnswer()"> Get Answer</button>
         <button class=" btn btn-outline-success" onclick="app.valuesController.getNewQuestion()">New Question</button>
